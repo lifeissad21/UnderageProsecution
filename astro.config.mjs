1127310f1497faaf9ebb1dc870163cee3ffa7afe
George Mathew <nerd2923@gmail.com>
2026-03-11 19:35:51 -0400

    Added test deployment for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://lifeissad21.github.io",
+  base: "/UnderageProsecutionWebsite",
   vite: {
     plugins: [tailwindcss()]
   }
